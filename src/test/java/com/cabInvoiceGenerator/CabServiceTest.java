4e3ce46e5fe619dba214d6cf7ab016a0efa29347
Minal <patilminalp@gmail.com>
2021-07-23 02:51:23 +0530

    Added:Find Total fare cost of Journey

--- src/test/java/com/cabInvoiceGenerator/CabServiceTest.java
+++ src/test/java/com/cabInvoiceGenerator/CabServiceTest.java
@@ -6,17 +6,26 @@ import org.junit.Test;
 
 public class CabServiceTest {
 
-   
+	@Test
 
-    @Test
+	public void InvoiceGenratorShowIdReturnTotalFare() {
+		CabService cabService = new CabService();
+		double distance = 10;
+		int time = 15;
+		double totalFare = cabService.invoiceGenrator(distance, time);
+		assertEquals(115.0, totalFare, 0.0);
 
-    public void InvoiceGenratorShowIdReturnTotalFare() {
-        CabService cabService = new CabService();
-        double distance = 10;
-        int time = 15;
-        double totalFare = cabService.invoiceGenrator(distance, time);
-        assertEquals(115.0, totalFare, 0.0);
+	}
 
-    }
+	@Test
+
+	public void InvoiceGenratorShowIdReturn_DefaultFare() {
+		CabService cabService = new CabService();
+		double distance = 0.600;
+		int time = 1;
+		double totalFare = cabService.invoiceGenrator(distance, time);
+		assertEquals(5.0, totalFare, 0.0);
+
+	}
 
 }
